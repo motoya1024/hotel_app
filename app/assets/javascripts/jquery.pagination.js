f210a34767369176745474f666fe72dfbe4fc9ce
motoyaabe <abetty9750@yahoo.co.jp>
2019-07-19 08:51:04 +0000

    fixed jquery_paginati o

--- app/assets/javascripts/jquery.pagination.js
+++ app/assets/javascripts/jquery.pagination.js
@@ -43,31 +43,9 @@
 				 	}else {$(this).show();} 				//else fade in 
 				 }); 										// end of for each tr in table
 					});										// end of on click pagination list
-
-
 		});
 
 												// end of on select change 
-		 
-								
-
 								// END OF PAGINATION 
 	}	
 
-
-
-
-
-
-
-$(function(){
-	// Just to append id number for each row  
-					$('table tr:eq(0)').prepend('<th> ID </th>')
-
-					var id = 0;
-
-					$('table tr:gt(0)').each(function(){	
-						id++
-						$(this).prepend('<td>'+id+'</td>');
-					});
-})
