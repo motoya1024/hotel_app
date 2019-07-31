5e7555f072f97e84aa36fc47dc2110728426cf07
motoyaabe <abetty9750@yahoo.co.jp>
2019-07-31 11:15:23 +0000

    fixed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,6 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
         log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         if user.admin == nil
            redirect_to myhotel_path(current_user)
         else 
