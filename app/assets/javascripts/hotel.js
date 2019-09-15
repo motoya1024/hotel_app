9af4e6c11fdfee5f4b4a195c8f819ffd33249d51
motoyaabe <abetty9750@yahoo.co.jp>
2019-09-15 11:27:07 +0000

    sp_add_homepage

--- app/assets/javascripts/hotel.js
+++ app/assets/javascripts/hotel.js
@@ -9,7 +9,7 @@ $(document).on("turbolinks:load", function() {
   });
   
    // 入力文字数
-  $(document).on("input", ".letter", function (e) {
+  $(document).on("keyup change", ".letter", function (e) {
     var txt = $(this).val();
     var new_txt = $.trim(txt.replace(/\n/g, ""));
     var count = new_txt.length;
